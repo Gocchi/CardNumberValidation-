0ae5e665aff6c917cbdd22b8f7272cc90d1176c9
Ilse Centeno <Ilse Centeno>
2017-04-09 23:07:28 -0500

    segunda version

--- ejercicio-01.js
+++ ejercicio-01.js
@@ -1,26 +1,32 @@
 function isValidCard(creditCardNumber) {
     // Escribe tu código aquí
+    if(typeof(creditCardNumber)!== 'number'){
+    	return "Error de dato"
+    }
+    if (creditCardNumber.toString().length!==16){
+    	return "Faltan numeros";
+    }    
+    
     var sumaPar=0;
 	var sumaImpar=0;
 	for(var i=0;i<creditCardNumber.length;i++){
 		if(i%2===0){
-			if(parseInt(creditCardNumber.substring(i,i+1))*2 > 9){
-			sumaImpar+=parseInt(creditCardNumber.substring(i,i+1))*2-9;
+			if(Number(creditCardNumber.substring(i,i+1))*2 > 9){
+			sumaImpar+=Number(creditCardNumber.substring(i,i+1))*2-9;
 			}
-			else{
-				sumaImpar+=parseInt(creditCardNumber.substring(i,i+1))*2;
+			if(sumaImpar+=Number(creditCardNumber.substring(i,i+1))*2);
 			}
-			sumaPar+=parseInt(creditCardNumber.substring(i,i+1));	
+			if(sumaPar+=Number(creditCardNumber.substring(i,i+1)));	
 			
 			
 	}
 	var suma = sumaPar+sumaImpar;
 	var MOD=suma%10;
 	if(MOD===0){
-		alert (true);
+		return true;
 	}
 	else
-		alert(false);
+		return false;
 
 }
 }
